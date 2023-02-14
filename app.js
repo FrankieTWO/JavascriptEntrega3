2de237473e1708009a0ad44892249d974a33a994
Franco Padovani <franpadovani@gmail.com>
2023-02-14 17:05:41 -0300

    nuevos cambios en local storage e items

--- app.js
+++ app.js
@@ -4,14 +4,48 @@
     { id: 301, nombre: "triple", precio: 300, cantidad: 1,  disponible: true, imgUrl:"./images/habTripleFoto.jpg" },
     { id: 401, nombre: "suite", precio: 400, cantidad: 1,  disponible: true, imgUrl: "./images/habTripleFoto.jpg" }
 ]
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [] ;
+// let carrito = JSON.parse(localStorage.getItem("carrito")) || [] ;
+
+
+let carrito = [];
 let contenedorProductos = document.getElementById("contenedorProductos")
 const carritoContenedor = document.querySelector("#carritoContenedor")
 const vaciarCarrito = document.querySelector("#vaciarCarrito")
+const precioTotal = document.getElementById("precioTotal")
+const procesarCompra = document.getElementById("procesarCompra")
+let carritoGuardado = []
+if (localStorage.getItem("carrito")) {
+  carritoGuardado = JSON.parse(localStorage.getItem("carrito"))
+  carritoGuardado = carrito
+  
+  
+} 
+for (const item of carritoGuardado) {
+  console.log(carrito)
+  mostrarCarrito
+  
+  
+}
+
+procesarCompra.addEventListener("click", () => {
+  if (carrito.length === 0){
+    swal({
+        title: "¡Tu carrito está vacio!",
+        text: "Compra algo para continuar con la compra",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+
+  })
+}  else {
+  // ACA PASA ALGO CUANDO COMPRAS 
+}
+
+})
 
 if (vaciarCarrito) {
   vaciarCarrito.addEventListener("click", () => {
     carrito.length = [];
+    localStorage.clear("carrito")
     mostrarCarrito();
   });
 }
@@ -25,9 +59,26 @@ function eliminarProducto(id){
 
 
 function agregarProducto(id){
-  const item = productos.find((prod) => prod.id === id)
-  carrito.push(item)
-  localStorage.setItem("carrito" , JSON.stringify(carrito));
+  const existe = carrito.some(prod => prod.id == id)
+  if (existe){
+    const prod = carrito.map(prod => {
+      if(prod.id === id ){
+        prod.cantidad++
+        console.log(carrito + "doble cantidad")
+      }
+    })
+  }else {
+    const item = productos.find((prod) => prod.id === id)
+    carrito.push(item)
+    carritoGuardado.push(item)
+    console.log(carrito + "agregado al carrito" )
+    localStorage.setItem("carrito", JSON.stringify(carritoGuardado))
+  
+    
+
+  }
+
+  
   mostrarCarrito()
 
 }
@@ -37,7 +88,7 @@ for (const producto of productos){
    const botones = document.getElementById("agregar${producto.id}")
    tarjetaProducto.className = 'producto'
    tarjetaProducto.innerHTML = `
-   <div class="card" style="width: 18rem;">
+   <div class="card" style="width: auto;">
       <img src="${producto.imgUrl}" class="card-img-top" alt="Imagenes de las habitacioes">
       <div class="card-body">
       <h5 class="card-title">${producto.nombre}</h5>
@@ -73,20 +124,23 @@ const mostrarCarrito = () => {
         <p>Producto: ${nombre}</p>
       <p>Precio: ${precio}</p>
       <p>Disponible :${disponible}</p>
+      <p>Cantidad :${cantidad}</p>
       <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Cancelar Habitacion</button>
         </div>
       </div>`;
     });
   }
   if (carrito.length === 0) {
-    console.log("Nada");
+    
     modalBody.innerHTML = `
     <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
     `;
   } else {
-    console.log("Algo");
+    // AÑADISTE UN ITEM AL CARRITO 
   }
-  carritoContenedor.textContent = carrito.length;
+  carritoContenedor.innerText = carrito.length;
+
+  precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
 
 }
 
