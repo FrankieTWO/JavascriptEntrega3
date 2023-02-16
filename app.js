9dc4abacfad5134c0395d76fe7a63851dd89b77e
Franco Padovani <franpadovani@gmail.com>
2023-02-16 18:51:06 -0300

    fetch y stock en JSON

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
- const productos = [
-    { id: 101, nombre: "simple", precio: 100, cantidad: 1, disponible: true, imgUrl:"./images/habDobleFoto.jpg" },
-    { id: 201, nombre: "doble", precio: 200, cantidad: 1,  disponible: true, imgUrl:"./images/habSuiteFoto.jpg" },
-    { id: 301, nombre: "triple", precio: 300, cantidad: 1,  disponible: true, imgUrl:"./images/habTripleFoto.jpg" },
-    { id: 401, nombre: "suite", precio: 400, cantidad: 1,  disponible: true, imgUrl: "./images/habTripleFoto.jpg" }
-]
-// let carrito = JSON.parse(localStorage.getItem("carrito")) || [] ;
+//   const productos = [
+//      { id: 101, nombre: "simple", precio: 100, cantidad: 1, disponible: true, imgUrl:"./images/habDobleFoto.jpg" },
+//      { id: 201, nombre: "doble", precio: 200, cantidad: 1,  disponible: true, imgUrl:"./images/habSuiteFoto.jpg" },
+//      { id: 301, nombre: "triple", precio: 300, cantidad: 1,  disponible: true, imgUrl:"./images/habTripleFoto.jpg" },
+//      { id: 401, nombre: "suite", precio: 400, cantidad: 1,  disponible: true, imgUrl: "./images/habTripleFoto.jpg" }
+//  ]
+
 
 
 let carrito = [];
+let carritoGuardado = [];
 let contenedorProductos = document.getElementById("contenedorProductos")
 const carritoContenedor = document.querySelector("#carritoContenedor")
 const vaciarCarrito = document.querySelector("#vaciarCarrito")
 const precioTotal = document.getElementById("precioTotal")
 const procesarCompra = document.getElementById("procesarCompra")
-let carritoGuardado = []
 
-const productoGuardado = localStorage.getItem('carrito');
-console.log(productoGuardado)
-if (productoGuardado) {
-  const carritoContenido = JSON.parse(productoGuardado);
-  const carritoLongitud = carritoContenido.length;
-  carritoContenedor.textContent = carritoLongitud;
-} 
-for (const item of carritoGuardado) {
-  console.log(carrito)
-  mostrarCarrito
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((arrayProductos) => mostrarCarrito(arrayProductos));
+  
+
+
+carritoContenedor.addEventListener("click", () => {
+  mostrarCarrito();
+});
+ const productoGuardado = localStorage.getItem('carrito');
+ console.log(productoGuardado)
+ if (productoGuardado) {
+   const carritoContenido = JSON.parse(productoGuardado);
+   const carritoLongitud = carritoContenido.length;
+   carritoContenedor.textContent = carritoLongitud;
+ } 
+ for (const item of carritoGuardado) {
+   console.log(carrito)
+   mostrarCarrito
   
   
-}
+ }
 
 procesarCompra.addEventListener("click", () => {
   if (carrito.length === 0){
@@ -47,7 +56,7 @@ procesarCompra.addEventListener("click", () => {
 if (vaciarCarrito) {
   vaciarCarrito.addEventListener("click", () => {
     carrito.length = [];
-    localStorage.clear("carrito")
+    localStorage.clear()
     mostrarCarrito();
   });
 }
@@ -55,53 +64,35 @@ if (vaciarCarrito) {
 function eliminarProducto(id){
   const habitacionId = id
   carrito =carrito.filter((hab) => hab.id !== habitacionId)
-  localStorage.setItem("carrito", JSON.stringify(carrito));
   mostrarCarrito()
 }
 
 
 function agregarProducto(id){
   const existe = carrito.some(prod => prod.id == id)
-  if (existe){
-    const prod = carrito.map(prod => {
+
+
+   if (existe){
+     const prod = carrito.map(prod => {
       if(prod.id === id ){
         prod.cantidad++
-      }
-      return prod;
-    });
-    carrito = [...prod];
-  } else {
-    const item = productos.find((prod) => prod.id === id)
-    item.cantidad = 1;
-    carrito.push(item);
-    carritoGuardado.push(item);
-    localStorage.setItem("carrito", JSON.stringify(carritoGuardado));
-  }
-  mostrarCarrito();
-}
-
-//   if (existe){
-//     const prod = carrito.map(prod => {
-//       if(prod.id === id ){
-//         prod.cantidad++
-//         console.log(carrito + "doble cantidad")
-//       }
-//     })
-//   }else {
-//     const item = productos.find((prod) => prod.id === id)
-//     carrito.push(item)
-//     carritoGuardado.push(item)
-//     console.log(carrito + "agregado al carrito" )
-//     localStorage.setItem("carrito", JSON.stringify(carritoGuardado))
+       }
+     })
+   }else {
+     const item = productos.find((prod) => prod.id === id)
+     carrito.push(item)
+     carritoGuardado.push(item)
+    console.log(carrito + "agregado al carrito" )
+     localStorage.setItem("carrito", JSON.stringify(carritoGuardado))
   
     
 
-//   }
+   }
 
   
-//   mostrarCarrito()
+   mostrarCarrito()
 
-// }
+ }
 
 for (const producto of productos){
    let tarjetaProducto = document.createElement('div')
@@ -127,56 +118,51 @@ for (const producto of productos){
   boton.addEventListener("click", () => agregarProducto(producto.id))
 }
 
-
-const mostrarCarrito = () => {
-  const modalBody = document.querySelector(".modal .modal-body");
-  if (modalBody) {
-    modalBody.innerHTML = "";
-    carrito.forEach((prod) => {
-      const { id, nombre, precio, disponible, imgUrl, cantidad } = prod;
-      modalBody.innerHTML += `
-      <div class="modal-contenedor">
-        <div>
-        <img class="img-fluid img-carrito" src="${imgUrl}"/>
-        </div>
-        <div>
-        <p>Producto: ${nombre}</p>
-      <p>Precio: ${precio}</p>
-      <p>Disponible :${disponible}</p>
-      <p>Cantidad :${cantidad}</p>
-      <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Cancelar Habitacion</button>
-        </div>
-      </div>`;
-    });
-  }
-  if (carrito.length === 0) {
+ const mostrarCarrito = () => {
+   const modalBody = document.querySelector(".modal .modal-body");
+   if (modalBody) {
+     modalBody.innerHTML = "";
+     carrito.forEach((prod) => {
+       const { id, nombre, precio, disponible, imgUrl, cantidad } = prod;
+       modalBody.innerHTML += `
+       <div class="modal-contenedor">
+         <div>
+         <img class="img-fluid img-carrito" src="${imgUrl}"/>
+         </div>
+         <div>
+         <p>Producto: ${nombre}</p>
+       <p>Precio: ${precio}</p>
+       <p>Disponible :${disponible}</p>
+       <p>Cantidad :${cantidad}</p>
+       <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Cancelar Habitacion</button>
+         </div>
+       </div>`;
+     });
+   }
+   if (carrito.length === 0) {
     
-    modalBody.innerHTML = `
-    <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
-    `;
-  } else {
-    // AÑADISTE UN ITEM AL CARRITO 
-    const productoGuardado = localStorage.getItem('carrito');
-    console.log(productoGuardado)
-    if (productoGuardado) {
-      const carritoContenido = JSON.parse(productoGuardado);
-      const carritoLongitud = carritoContenido.length;
-      carritoContenedor.textContent = carritoLongitud;
-    }
-      }
+    
+     modalBody.innerHTML = `
+     <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
+     `;
+   } else {
+     // AÑADISTE UN ITEM AL CARRITO 
+     const productoGuardado = localStorage.getItem('carrito');
+     carritoContenedor.innerText = carrito.length;
+     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+     console.log(productoGuardado)
       
-      precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+        carritoContenedor.textContent = carrito.length;
+     
+       }
       
-    }
+       
+      
+     }
 
 
 
-//   }
-//   carritoContenedor.innerText = carrito.length;
-
-//   precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
-
-// }
+ 
 
 
   
